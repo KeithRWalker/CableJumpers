0bc2df85187c4d4571932daf72d100142e79e142
Keith Walker <Keithrwalker418@gmail.com>
2019-09-21 03:17:18 -0500

    added

---
+++ KimberlyHart/Classes/Events/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CableJumpers.Classes.Hosts;
+
+namespace CableJumpers.Classes.Events
+{
+    class Event
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public List<Host> HostList { get; set; }
+
+
+        public Event(string name, int year)
+        {
+
+        }
+    }
+}
---
+++ KimberlyHart/Classes/Wallet/Wallet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CableJumpers.Classes.Wallet
+{
+    class Wallet
+    {
+    }
+}
